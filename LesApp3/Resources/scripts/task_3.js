596b534b5661f359bbfe5fd552a4c3f2021aec23
Bogdan <pinchuk.brus@windowslive.com>
2021-07-05 23:52:48 +0300

    Added paer 12 les 2 task 3

---
+++ LesApp3/Resources/scripts/task_3.js
@@ -0,0 +1,17 @@
+var data = [1, 2, 3, 4, 5];
+console.log(`Вхідні дані: ${data}`);
+
+// old
+/*
+var result = data.map(function (value) {
+    return value * value;
+});
+
+result.forEach(function (e) {
+    console.log(e);
+});
+*/
+
+// заміна на стрілочну функцію (лямда вираз)
+var result = data.map(value => value ** 2);
+result.forEach(e => console.log(e));
